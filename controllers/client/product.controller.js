e0c000bda4b1d8df57c9a2e5a20228988842996e
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-08-01 01:54:43 +0700

    product list

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model');
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    
+    const newProducts = products.map(product => {
+        product.priceNew = (product.price - (product.price * product.discountPercentage / 100)).toFixed(2);
+        return product;
+    })
+    
     res.render('client/pages/products/index', {
-        pageTitle: 'Trang danh sách sản phẩm'
+        pageTitle: 'Danh sách sản phẩm',
+        products: newProducts
     })
 }
