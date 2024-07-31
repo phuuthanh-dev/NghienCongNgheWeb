def49fde5d57156ee96bbc20061ed44c0fe49371
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-08-01 03:43:20 +0700

    render admin list products from database

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model');
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+
     res.render('admin/pages/products/index', {
-        pageTitle: 'Danh sách sản phẩm'
+        pageTitle: 'Danh sách sản phẩm',
+        products: products
     })
 }
