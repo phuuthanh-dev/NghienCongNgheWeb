2b86aa9b9ad6167344e087469d765ea5511e3c6d
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-08-05 23:44:00 +0700

    create collection roles

--- validates/admin/product-category.validate.js
+++ validates/admin/product-category.validate.js
@@ -1,6 +1,4 @@
 module.exports.createPost = (req, res, next) => {
-    console.log(req.body);
-    
     if (!req.body.title) {
         req.flash('error', 'Vui lòng nhập tiêu đề!')
         res.redirect('back')
