f92ba526f7773e0a1d7463f2b584fa35d40a9930
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-08-06 23:28:23 +0700

    edit account

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -9,7 +9,7 @@ const systemConfig = require("../../config/system")
 module.exports.index = async (req, res) => {
     const accounts = await Account.find({
         deleted: false
-    });
+    }).select("-password -token");
 
     for (const account of accounts) {
         const role = await Role.findOne({
@@ -58,4 +58,66 @@ module.exports.createPost = async (req, res) => {
 
     req.flash("success", "Tạo tài khoản thành công");
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+}
+
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const account = await Account.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        const roles = await Role.find({
+            deleted: false
+        }).select("title");
+
+        res.render("admin/pages/accounts/edit", {
+            pageTitle: "Chinh sửa tài khoản",
+            roles: roles,
+            account: account
+        });
+    }
+    catch (error) {
+        req.flash("error", "Tài khoản không tồn tại");
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+    }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const emailExist = await Account.findOne({
+            _id: { $ne: id },
+            email: req.body.email,
+            deleted: false
+        });
+
+        if (emailExist) {
+            req.flash("error", `Email ${emailExist.email} đã tồn tại`);
+            res.redirect("back");
+            return;
+        }
+
+        if (req.body.password == "") {
+            delete req.body.password;
+        } else {
+            req.body.password = md5(req.body.password);
+        }
+
+        await Account.updateOne({
+            _id: id,
+            deleted: false
+        }, req.body);
+
+        req.flash("success", "Cập nhật thành công!");
+    } catch (error) {
+        req.flash("error", "Tài khoản không tồn tại");
+    }
+    res.redirect("back");
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -12,13 +12,10 @@ router.get("/", controller.index)
 
 router.get("/create", controller.create)
 
-router.post(
-  "/create",
-  upload.single('avatar'),
-  uploadCloud.upload,
-  validate.createPost,
-  controller.createPost
-);
+router.post("/create", upload.single('avatar'), uploadCloud.upload, validate.createPost, controller.createPost);
 
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id", upload.single('avatar'), uploadCloud.upload, validate.editPatch, controller.editPatch);
 
 module.exports = router;
